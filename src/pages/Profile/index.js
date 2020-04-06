ef47b6ee21f8826ed6d77f17168ea2781e9f9e52
Juan Jose Rodriguez <jjoserodriguez@live.com>
2020-04-05 21:56:56 -0300

    refactor(profile and maintemplate pages): delete bootstrap references

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,41 +1,31 @@
 import React from "react";
 import { connect } from 'react-redux';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 
-import Container from "react-bootstrap/Container";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Table from 'react-bootstrap/Table';
+import { makeStyles } from '@material-ui/core/styles';
+
 
 function Profile({ currentUser }) {
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      },
+    }
+  ));
+
+
+  const classes = useStyles();
   return (
-    <Container>
-      <Row className="justify-content-center mt-5">
-        <Col xs={4}><h1 className='text-center'>Profile</h1></Col>
-      </Row>
-      <Row className="justify-content-center mt-2">
-        <Col xs={4}>
-          <p className="font-weight-bold text-center">{currentUser.profile.email}</p>
-        </Col>
-      </Row>
-      <Row className="justify-content-center mt-1">
-        <Col xs={4}>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th className='text-center'>Car ID</th>
-            </tr>
-          </thead>
-          <tbody>
-            {currentUser.profile.cars.map(car =>(
-              <tr key={car}>
-                <td key={car} className='text-uppercase text-center'>
-                  {car}
-                </td>
-              </tr>))}
-            </tbody>
-          </Table>  
-        </Col>
-      </Row>
+    <Container component="main" maxWidth="xs">      
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+        Profile
+        </Typography>   
+      </div>   
     </Container>
   );
 };
--- src/templates/MainTemplate/index.js
+++ src/templates/MainTemplate/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import NavBar from '../../components/Navbar';
 import Footer from '../../components/Footer';
-import Container from 'react-bootstrap/Container';
+import Container from '@material-ui/core/Container';
+
 
 type PropsType = {
   children: React$Node
