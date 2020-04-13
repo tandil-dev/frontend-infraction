184fdc0e8704645899eb0e44b96b3ea3b7d5a9a5
fedelmar <fsuarez91@gmail.com>
2020-04-13 18:54:54 -0300

    Add new video

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,27 +1,50 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
+import { Grid } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import ReactPlayer from 'react-player';
 import Card from '@material-ui/core/Card';
 import useStyles from './style';
+import './videoStyle.css';
 
 export default function Home() {
   const classes = useStyles();
   return (
-    <Container component="main" maxWidth="xs">
-      <div className={classes.paper}>
-        <Typography component="h1" variant="h5">
-          Home
-        </Typography>
-      </div>
-      <Card>
-        <ReactPlayer
-          url="https://youtu.be/YVgfHZMFFFQ"
-          playing
-          width="100%"
-          height="100%"
-        />
-      </Card>
-    </Container>
+    <>
+      <Container component="main" maxWidth="xs">
+        <div className={classes.paper}>
+          <Typography component="h1" variant="h5">
+            Home
+          </Typography>
+        </div>
+      </Container>
+      <Grid
+        container
+        justify="space-between"
+      >
+        <Grid item sm>
+          <Card className="player-wrapper">
+            <ReactPlayer
+              className="react-player"
+              url="https://youtu.be/70asKwy15Ds"
+              playing
+              width="100%"
+              height="100%"
+            />
+          </Card>
+        </Grid>
+        <Grid item sm>
+          <Card className="player-wrapper">
+            <ReactPlayer
+              className="react-player"
+              url="https://youtu.be/YVgfHZMFFFQ"
+              playing
+              width="100%"
+              height="100%"
+            />
+          </Card>
+        </Grid>
+      </Grid>
+    </>
   );
 }
