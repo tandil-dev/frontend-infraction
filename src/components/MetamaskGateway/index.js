e7f9ce4b0a1fba1e5677c66d25bcf97325a352ec
fedelmar <fsuarez91@gmail.com>
2020-04-22 20:38:53 -0300

    Delete console logs

--- src/components/MetamaskGateway/index.js
+++ src/components/MetamaskGateway/index.js
@@ -3,11 +3,6 @@ import MetamaskDownload from './MetamaskDownload';
 
 const MetamaskGateway = ({ children }) => {
   const isWeb3Browser = !!window.ethereum;
-  if (isWeb3Browser) {
-    console.log('Hay metamask');
-  } else {
-    console.log('No hay metamask');
-  }
 
   return isWeb3Browser
     ? children
