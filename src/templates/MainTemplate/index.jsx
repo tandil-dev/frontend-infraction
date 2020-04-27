2c3b0969cac3f1f4e6b948c82931b27434637e45
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2020-04-27 17:56:39 -0300

    fix(styles): remove Container from Main template

--- src/templates/MainTemplate/index.jsx
+++ src/templates/MainTemplate/index.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
 import NavBar from '../../components/Navbar';
 import Footer from '../../components/Footer';
 
 const MainTemplate = ({ children, appProps }) => (
-  <Container>
+  <>
     <NavBar {...appProps} />
     {children}
     <Footer />
-  </Container>
+  </>
 );
 
 export default MainTemplate;
