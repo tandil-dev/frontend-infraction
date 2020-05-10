b76abf5eb039762bdf09f3847d8df125048be27c
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2020-05-10 03:45:32 -0300

    chore(now): update config

--- src/config.js
+++ src/config.js
@@ -1,14 +1,10 @@
 /* eslint-disable eqeqeq */
 /* eslint-disable no-undef */
 
-const chainId = (window.ethereum && ethereum.chainId) || 3;
-const network = chainId == 20
-  ? process.env.REACT_APP_LOCAL_NETWORK
-  : process.env.REACT_APP_ROPSTEN_NETWORK;
-const smartContrAddress = chainId == 20
-  ? process.env.REACT_APP_SMART_CONTRACT_ADDRESS_LOCAL
-  : process.env.REACT_APP_SMART_CONTRACT_ADDRESS_ROPSTEN;
-const initialBlock = chainId == 20 ? 1 : 7604005;
+// const chainId = (window.ethereum && ethereum.chainId) || 3;
+const network = process.env.REACT_APP_ROPSTEN_NETWORK;
+const smartContrAddress = process.env.REACT_APP_SMART_CONTRACT_ADDRESS_ROPSTEN;
+const initialBlock = 7604005;
 
 export {
   network,
