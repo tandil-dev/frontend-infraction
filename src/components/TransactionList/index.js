8769389531c42ca3995b2e35e22f9c91c3351292
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2020-05-20 17:38:52 -0300

    feat(create infraction): add ipfs, tx send and tx list

--- src/components/TransactionList/index.js
+++ src/components/TransactionList/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
+/* eslint-disable no-restricted-syntax */
+import React, { useState, useEffect } from 'react';
+import { withRouter, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { useSubspace } from '@embarklabs/subspace-react';
 import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import CommentIcon from '@material-ui/icons/Comment';
+import {
+  List, ListItem, ListItemIcon, ListItemText, CircularProgress, Button,
+} from '@material-ui/core';
+
+import CheckIcon from '@material-ui/icons/Check';
+
+import ipfs from '../../web3/ipfs';
+import { abi, infractionFactoryAddress } from '../../web3/infractionFactory';
+import { infractionsData } from './infractions';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,47 +22,83 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function TransactionList() {
+function TransactionList({ currentReport }) {
+  const [checked, setChecked] = useState(0);
   const classes = useStyles();
-  const [checked, setChecked] = React.useState([0]);
+  const [ipfsHash, setIpfsHash] = useState(undefined);
+  const subspace = useSubspace();
+  const [infractionFactory, setInfractionFactory] = useState(null);
 
-  const handleToggle = (value) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
+  async function saveToIpfs(reportData) {
+    try {
+      const ipfs2 = await ipfs; // ipfs is a promise here, need to await
+      const result = [];
+      for await (const resultPart of ipfs2.add([JSON.stringify(reportData)])) {
+        result.push(resultPart);
+      }
+      setIpfsHash(result[0].path);
+      setChecked(1);
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(e);
     }
+  }
 
-    setChecked(newChecked);
+  useEffect(() => {
+    if (ipfsHash || !currentReport) return;
+    saveToIpfs(currentReport);
+  }, [currentReport]);
+
+  useEffect(() => {
+    if (infractionFactory) return;
+    setInfractionFactory(subspace.contract({ abi, address: infractionFactoryAddress }));
+  }, [subspace]);
+
+  const sendTx = async () => {
+    if (!infractionFactory) return;
+
+    infractionFactory.methods
+      .createInfraction(ipfsHash, currentReport.infractionVideo, currentReport.imagenDominio)
+      .send({ from: subspace.web3.eth.defaultAccount, gasLimit: 2100000 })
+      .then((r) => {
+        // eslint-disable-next-line no-console
+        console.log(r);
+        setChecked(3);
+      })
+      // eslint-disable-next-line no-console
+      .catch((e) => console.log(e));
+
+    setChecked(2);
   };
 
+  useEffect(() => {
+    if (!infractionFactory || !currentReport || !ipfsHash) return;
+    sendTx();
+  }, [ipfsHash, infractionFactory, currentReport]);
+
   return (
-    <List className={classes.root}>
-      {[0, 1, 2, 3].map((value) => {
-        const labelId = `checkbox-list-label-${value}`;
-        return (
-          <ListItem key={value} role={undefined} dense button onClick={handleToggle(value)}>
-            <ListItemIcon>
-              <Checkbox
-                edge="start"
-                checked={checked.indexOf(value) !== -1}
-                tabIndex={-1}
-                disableRipple
-                inputProps={{ 'aria-labelledby': labelId }}
-              />
-            </ListItemIcon>
-            <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
-            <ListItemSecondaryAction>
-              <IconButton edge="end" aria-label="comments">
-                <CommentIcon />
-              </IconButton>
-            </ListItemSecondaryAction>
-          </ListItem>
-        );
-      })}
-    </List>
+    <>
+      <List className={classes.root}>
+        {infractionsData.map((infraction, index) => {
+          const labelId = infraction.text.replace(/ /g, ''); // Removes spaces
+          return (
+            <ListItem key={infraction.text} role={undefined} dense button>
+              <ListItemIcon>
+                {index < checked ? <CheckIcon /> : <CircularProgress />}
+              </ListItemIcon>
+              <ListItemText id={labelId} primary={infraction.text} />
+            </ListItem>
+          );
+        })}
+      </List>
+      { checked[0] && checked[1]
+        && (<Button component={Link} color="primary" to="/" fullWidth>Terminar</Button>)}
+    </>
   );
 }
+
+const mapStateToProps = (state) => ({
+  currentReport: state.currentReport.data,
+});
+
+export default connect(mapStateToProps)(withRouter(TransactionList));
---
+++ src/components/TransactionList/infractions.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/prefer-default-export */
+
+export const infractionsData = [
+  {
+    text: 'Guardando datos en IPFS',
+  },
+  {
+    text: 'Enviando nueva infracción',
+  },
+  {
+    text: 'Infracción creada',
+  },
+];
