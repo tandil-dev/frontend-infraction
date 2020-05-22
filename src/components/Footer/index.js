9bde80d0637987c05d71ed81925401d044de2bf4
Franco D. Berdun <fberdun@gmail.com>
2020-05-22 21:09:51 +0200

    add icon camera, icon profile & icon menu

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,9 +3,17 @@ import React from 'react';
 import { withRouter, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
+import { AppBar } from '@material-ui/core';
 import useStyles from './style';
 import MetamaskGateway from '../MetamaskGateway';
+
+
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import AddAPhoto from '@material-ui/icons/AddAPhoto';
+
 
 
 const Footer = ({ currentUser }) => {
@@ -15,9 +23,36 @@ const Footer = ({ currentUser }) => {
   return (
     <>
       {isLogedIn && (
-        <MetamaskGateway>
-          <Fab color="primary" aria-label="add" className={classes.fab} component={Link} to="report"><AddIcon /></Fab>
-        </MetamaskGateway>
+        <AppBar position="fixed" color="primary" className={classes.appBar}>
+          <Toolbar>
+
+            <MetamaskGateway>
+              <IconButton edge="start" color="inherit">
+                <Fab color="secondary" aria-label="add" className={classes.fabButton} component={Link} to="report">
+                  <AddAPhoto />
+                </Fab>
+              </IconButton>
+            </MetamaskGateway>
+
+            <div className={classes.grow} />
+
+            <IconButton color="inherit">
+              <Fab color="secondary" aria-label="add" className={classes.fabButton}>
+                <AccountCircle />
+              </Fab>
+            </IconButton>
+
+            <div className={classes.grow} />
+
+            <IconButton edge="end" color="inherit">
+              <Fab color="secondary" aria-label="add" className={classes.fabButton}>
+                <MenuIcon />
+              </Fab>
+            </IconButton>
+
+          </Toolbar>
+        </AppBar>
+
       )}
     </>
   );
--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -6,6 +6,24 @@ const useStyles = makeStyles((theme) => ({
     bottom: theme.spacing(3),
     right: theme.spacing(3),
   },
+  text: {
+    padding: theme.spacing(2, 2, 0),
+  },
+  appBar: {
+    top: 'auto',
+    bottom: 0,
+  },
+  fab: {
+    position: 'absolute',
+    bottom: theme.spacing(3),
+    right: theme.spacing(3),
+  },
+  typography1: {
+    padding: theme.spacing(1, 0, 1, 1),
+  },
+  typography2: {
+    padding: theme.spacing(0, 0, 1, 1),
+  },
 
 }));
 
