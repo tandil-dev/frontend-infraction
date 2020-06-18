121478a804bfe7557033196844bb2f37a235e8b4
fedelmar <fsuarez91@gmail.com>
2020-06-18 20:22:45 -0300

    Agregar lista de posibles infracciones

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
 import {
-  Card, CardMedia, Grid, Typography,
+  Card, CardMedia, Grid, Typography, List, ListItemText,
 } from '@material-ui/core';
 import UserTypes from '../../components/UserTypes';
 import useStyles from './styles';
@@ -39,11 +39,26 @@ export default function Home() {
           spacing={2}
         >
           <Grid item xs={12}>
-            <Typography variant="h4" component="h2" className={classes.header}>
+            <Typography variant="h5" component="h2" className={classes.header}>
               ¿A quién está dirigida la aplicación?
             </Typography>
           </Grid>
           <UserTypes />
+          <Grid item xs={12}>
+            <Typography variant="h6" component="h2" className={classes.header}>
+              ¿Qué denuncias puedo hacer?
+            </Typography>
+            <List component="nav">
+              <ListItemText primary="Estacionamiento en cordón amarillo." />
+              <ListItemText primary="Obstruye rampa/espacio reservado." />
+              <ListItemText primary="Estaciona obstruyendo senda peatonal." />
+              <ListItemText primary="Estaciona obstruyendo ochava." />
+              <ListItemText primary="Estaciona sobre la senda peatonal." />
+              <ListItemText primary="Estacionamiento para bicicletas." />
+              <ListItemText primary="Estacionamiento sobre espacios verdes." />
+              <ListItemText primary="Estaciona en doble o triple fila." />
+            </List>
+          </Grid>
           <Grid item sm={6} xs={12}>
             <ReactPlayer
               url="https://youtu.be/15jzJiw7gJw"
