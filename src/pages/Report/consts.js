fd7978d6a51db46d02fceb11589626346c14d632
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2020-06-18 21:17:44 -0300

    fix(report): date, Time and infraction types

--- src/pages/Report/consts.js
+++ src/pages/Report/consts.js
@@ -13,3 +13,31 @@ export const MAP = {
   HEIGHT: '707',
   URL: '/map.png',
 };
+
+export const infractionTypes = [
+  'Circula de noche sin luces',
+  'Estaciona doble o triple fila',
+  'Estaciona en cordón amarillo',
+  'Estaciona en zona restringida fuera de horario',
+  'Estaciona obstruyendo bici senda',
+  'Estaciona obstruyendo garage',
+  'Estaciona obstruyendo ochava',
+  'Estaciona obstruyendo rampa y/o espacio reservado',
+  'Estaciona obstruyendo senda aeróbica',
+  'Estaciona obstruyendo senda peatonal',
+  'Estaciona obstruyendo vereda',
+  'Gira a la izquierda en avenida',
+  'Gira sin luces',
+  'Pasa semaforo en rojo',
+  'Sobrepasa en ruta con línea amarilla',
+  'Uso de celular en movimiento',
+];
+
+export const motoTypes = [
+  'Motociclista sin casco',
+  'Motociclista exedido de pasajeros',
+];
+
+export const otherTypes = [
+  'Otras infracciones',
+];
