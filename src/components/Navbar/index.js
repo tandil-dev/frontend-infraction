d9f11c7be25f52619006191428c13fb5a7e9b249
ezequiel <ezequiel.avalos@gmail.com>
2020-07-07 17:36:57 -0300

    fixes app bar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -24,7 +24,7 @@ const useStyles = makeStyles((theme) => ({
     marginRight: theme.spacing(2),
     marginLeft: theme.spacing(-1),
   },
-  loginButton:{
+  Button:{
     textTransform: 'none'
   },
 }));
@@ -49,10 +49,20 @@ const StyledNavbar = ({ logout, history, currentUser }) => {
         {isLogedIn
           ? (
             <>
-              <Button color="inherit" className={classes.loginButton} onClick={handleLogout}>Log out</Button>
+              <Button color="inherit"  className={classes.Button} size="large" onClick={handleLogout}>
+                <Typography variant="h5">
+                  Salir
+                </Typography>
+              </Button>
             </>
           )
-          : <Button component={Link} className={classes.loginButton} color="inherit" to="/login">Ingresar</Button>}
+          : (
+            <Button component={Link}  className={classes.Button} color="inherit" size="large" to="/login" >
+              <Typography variant="h5">
+                Ingresar
+              </Typography>
+            </Button>
+          )}
       </Toolbar>
     </AppBar>
   );
